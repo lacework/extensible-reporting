a43deba961051fa0e00d54fc21dbc081d8fc7dc6
Jeff Vogt <jvogt@users.noreply.github.com>
2022-09-13 16:51:22 -0700

    add sample jinja2 report

---
+++ reports/jinja2/host_vulns_report.py
@@ -0,0 +1,38 @@
+def generate_report(_shared, report_save_path, use_dummy_data):
+    import os
+    from datetime import datetime
+
+    import logging
+    logger = logging.getLogger(__name__)
+
+    import jinja2
+    
+    lw_provider = _shared.p_lw_dummy if use_dummy_data else _shared.p_lw
+
+    host_vulns = lw_provider.host_vulns(_shared._25_hours_ago, _shared._now)
+
+    # style
+    host_vulns_summary_by_host = _shared.t_lw.host_vulns_summary_by_host(host_vulns)
+    host_vulns_summary_by_host = host_vulns_summary_by_host.style.set_table_styles({"Severity Count" : [
+        {
+            "selector" :"td",
+            "props": "white-space: pre-wrap; text-align:left"
+        }
+    ]})
+
+    templateLoader = jinja2.FileSystemLoader(searchpath=os.path.dirname(__file__))
+    templateEnv = jinja2.Environment(loader=templateLoader, autoescape=True)
+    TEMPLATE_FILE = "host_vulns_report.html.j2"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    html = template.render(
+        host_vulns_total_evaluated=str(_shared.t_lw.host_vulns_total_evaluated(host_vulns)),
+        host_vulns_summary=_shared.t_lw.host_vulns_summary(host_vulns),
+        host_vulns_summary_by_host=host_vulns_summary_by_host,
+        host_vulns_full_table=_shared.t_lw.host_vulns_full_table(host_vulns),
+        date=datetime.now()
+    )
+
+    logger.info('Saving report to: ' + report_save_path)
+
+    with open(report_save_path, 'w') as file:
+        file.write(html)
