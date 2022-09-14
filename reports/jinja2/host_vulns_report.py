67b7ee87121157b5e973cf5ac25d8dd76d7d3f98
Jeff Vogt <jvogt@users.noreply.github.com>
2022-09-13 18:36:44 -0700

    add plotly bar graph example

--- reports/jinja2/host_vulns_report.py
+++ reports/jinja2/host_vulns_report.py
@@ -20,13 +20,18 @@ def generate_report(_shared, report_save_path, use_dummy_data):
         }
     ]})
 
+    host_vulns_summary = _shared.t_lw.host_vulns_summary(host_vulns)
+    host_vulns_summary_bar_graphic = _shared.g_lw_plotly.host_vulns_by_severity_bar(host_vulns_summary, width=720)
+    host_vulns_summary_bar_graphic = _shared.g_lw_plotly.bytes_to_image_tag(host_vulns_summary_bar_graphic)
+
     templateLoader = jinja2.FileSystemLoader(searchpath=os.path.dirname(__file__))
     templateEnv = jinja2.Environment(loader=templateLoader, autoescape=True)
     TEMPLATE_FILE = "host_vulns_report.html.j2"
     template = templateEnv.get_template(TEMPLATE_FILE)
     html = template.render(
         host_vulns_total_evaluated=str(_shared.t_lw.host_vulns_total_evaluated(host_vulns)),
-        host_vulns_summary=_shared.t_lw.host_vulns_summary(host_vulns),
+        host_vulns_summary_bar_graphic=host_vulns_summary_bar_graphic,
+        host_vulns_summary=host_vulns_summary,
         host_vulns_summary_by_host=host_vulns_summary_by_host,
         host_vulns_full_table=_shared.t_lw.host_vulns_full_table(host_vulns),
         date=datetime.now()
