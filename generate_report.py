6c0860581ce7432c4a9f62a505e6162caa5b4cf6
Jeff Vogt <jvogt@users.noreply.github.com>
2022-09-26 17:58:42 -0700

    change dummy to cached

--- generate_report.py
+++ generate_report.py
@@ -16,7 +16,7 @@ import importlib.util
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--report-path", help="Path to save report", default="report.html")
-    parser.add_argument("--use-dummy-data", help="Use dummy data (if available)", action='store_true')
+    parser.add_argument("--use-cached-data", help="Use cached data (if available)", action='store_true')
     parser.add_argument('report_generator', metavar='REPORT_GENERATOR', type=str, nargs=None,
                     help='Path to the report generator (eg: reports/datapane/compliance_report.py')
     args = parser.parse_args()
@@ -37,23 +37,23 @@ def main():
     class _shared:
         def __init__(self):
             import providers.lacework as p_lw
-            import providers.lacework_dummy as p_lw_dummy
+            import providers.lacework_cached as p_lw_cached
             import transformers.lacework as t_lw
             import graphics.lacework.plotly as g_lw_plotly
 
             self.p_lw = p_lw
-            self.p_lw_dummy = p_lw_dummy
+            self.p_lw_cached = p_lw_cached
             self.t_lw = t_lw
             self.g_lw_plotly = g_lw_plotly 
             self._now = datetime.now(timezone.utc)
             self._25_hours_ago = self._now - timedelta(hours = 25)
             self._7_days_ago = self._now - timedelta(days = 7)
-            self.use_dummy_data = args.use_dummy_data
+            self.use_cached_data = args.use_cached_data
             self.cli_data = {
                 'customer': 'AcmeCoPlaceHolder',
                 'author': 'John Q. Placeholder'
             }
 
-    report.generate_report(_shared(), report_save_path=args.report_path, use_dummy_data=args.use_dummy_data)
+    report.generate_report(_shared(), report_save_path=args.report_path, use_cached_data=args.use_cached_data)
 
 main()
