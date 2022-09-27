4c5a6465cae7f818c599973f231e4bf5b6dd138c
Jeff Vogt <jvogt@users.noreply.github.com>
2022-09-26 18:09:30 -0700

    aggregate severities by compliance account

--- transformers/lacework/compliance_reports_summary.py
+++ transformers/lacework/compliance_reports_summary.py
@@ -13,7 +13,9 @@ def compliance_reports_summary(compliance_reports, severities=["Critical", "High
     df = df[df['SEVERITY'].isin(severities)]
     
     df = df.groupby(['ACCOUNT_ID', 'SEVERITY']).agg(count=('SEVERITY', 'count'), resources=('RESOURCE_COUNT', 'sum'), assessed=('ASSESSED_RESOURCE_COUNT', 'sum')).reset_index() # .size().reset_index(name='count')
+    df['sev_merged'] = df['SEVERITY'].astype('string') + ": " + df['count'].astype('string')
 
-    df.rename(columns={'ACCOUNT_ID': 'Account ID', 'SEVERITY': 'Severity', 'count': 'Severity Count', 'resources': 'Non-compliant Resources', 'assessed': 'Total Assessed Resources'}, inplace=True)
+    df = df.groupby('ACCOUNT_ID').agg({'sev_merged' : f"\n".join, 'resources' : 'sum', 'assessed' : 'sum'}).reset_index()
+    df.rename(columns={'ACCOUNT_ID': 'Account ID', 'sev_merged': 'Severity Count', 'resources': 'Non-compliant Resources', 'assessed': 'Total Assessed Resources'}, inplace=True)
     
     return df
