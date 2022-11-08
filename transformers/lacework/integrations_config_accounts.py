ae14aef76d24b6489f62a39879c18eb070723dcb
Jeff Vogt <jeff.vogt@lacework.net>
2022-11-08 11:07:54 -0800

    slipping in a fix for empty aws compliance accts

--- transformers/lacework/integrations_config_accounts.py
+++ transformers/lacework/integrations_config_accounts.py
@@ -5,4 +5,6 @@ import numpy as np
 def integrations_config_accounts(integrations, types=["AWS_CFG", "AWS_CT_SQS"]):
     df = pd.json_normalize(integrations)
     df = df[(df['TYPE'] == "AWS_CFG") & (df['ENABLED'] == 1) & (df['STATE.ok'] == 1)]
+    if df.empty:
+        return []
     return df['DATA.AWS_ACCOUNT_ID'].unique().tolist()
