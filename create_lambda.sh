1bc4ad937215916f62b8083be2ad1bc251bba5f0
Tim MacDonald <tim.macdonald@lacework.net>
2023-06-28 15:49:00 -0500

    updated docker build and lambda

--- create_lambda.sh
+++ create_lambda.sh
@@ -3,5 +3,5 @@
 
 docker build -t csa_lambda .
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $1
-docker tag csa_lambda:latest $1/lwpartner:csa_lambda
-docker push $1/lwpartner:csa_lambda
+docker tag csa_lambda:latest $1/lwcsa:csa_lambda
+docker push $1/lwcsa:csa_lambda
