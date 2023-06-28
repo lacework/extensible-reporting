dbfe750a159a9f36ad33c5d8e502bd735e24226c
Tim MacDonald <tim.macdonald@lacework.net>
2023-06-28 13:46:32 -0500

    updated docker build and lambda

--- create_lambda.sh
+++ create_lambda.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
+
 docker build -t csa_lambda .
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 988001579975.dkr.ecr.us-east-2.amazonaws.com
-docker tag csa_lambda:latest 988001579975.dkr.ecr.us-east-2.amazonaws.com/lwpartner:csa_lambda
-docker push 988001579975.dkr.ecr.us-east-2.amazonaws.com/lwpartner:csa_lambda
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin $1
+docker tag csa_lambda:latest $1/lwpartner:csa_lambda
+docker push $1/lwpartner:csa_lambda
