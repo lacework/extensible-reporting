2f9c47dff6b99e1edfad384feb8a36b6ad68d0d0
Tim MacDonald <tim.macdonald@lacework.net>
2023-07-07 08:29:09 -0500

    updated lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -110,7 +110,9 @@ def lambda_handler(event, context):
     # generate pdf from html
     pdfkit_config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
     pdf_file_name = "/tmp/report.pdf"
-    pdfkit_options = {"enable-local-file-access": None}
+    pdfkit_options = {"enable-local-file-access": None,
+                      "viewport-size": "1920x1080",
+                      "disable-smart-shrinking": True}
     try:
         result = pdfkit.from_string(report, pdf_file_name, configuration=pdfkit_config, options=pdfkit_options, verbose=True)
     except Exception as e:
